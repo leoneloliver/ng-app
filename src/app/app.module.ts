41cded2d2d4b6e7709e48af5d27ff229abac9337
Leonel Oliveira - OnR <loliveira@hq.onresearch.com>
2019-01-08 09:37:35 -0500

    including search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { FilterPipe } from './filter.pipe';
+
 
 @NgModule({
   declarations: [
@@ -20,3 +24,6 @@ import { FilterPipe } from './filter.pipe';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
